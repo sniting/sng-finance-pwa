c2449bcc6edca9354c60096e1b563aefdc38ec9c
sniting <nitingokulsubramaniam@gmail.com>
2025-05-19 15:49:36 -0400

    Revert "Bootstrap React and move Firebase init"

--- src/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'https://unpkg.com/react@18/umd/react.development.js';
-import ReactDOM from 'https://unpkg.com/react-dom@18/umd/react-dom.development.js';
-import App from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('react-root')).render(<App />);
